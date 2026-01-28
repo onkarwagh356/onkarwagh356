0f5f7ce448419d481a66bbe1f2c53344bb2a917a
onkarwagh356 <153382697+onkarwagh356@users.noreply.github.com>
2026-01-28 23:47:24 +0530

    Add TOKENIZERS_PARALLELISM environment variable
    Set TOKENIZERS_PARALLELISM to false for compatibility.

--- Backend.py
+++ Backend.py
@@ -6,7 +6,7 @@ import numpy as np
 from langdetect import detect
 from flask_cors import CORS
 import os
-
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 app = Flask(__name__)
 CORS(app)
 
@@ -81,3 +81,4 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
